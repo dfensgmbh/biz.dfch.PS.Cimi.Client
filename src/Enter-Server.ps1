a109b5fc60e268e570739a25c3b93f5f5f361221
rufer7 <marc.rufer@d-fens.net>
2016-05-24 16:59:49 +0200

    Enter-Server.ps1
    * OAuthBaseUrl added to parameter sets
    * Logging improved

--- src/Enter-Server.ps1
+++ src/Enter-Server.ps1
@@ -61,7 +61,8 @@ Param
 (
 	# [Optional] The OAuthBaseUrl such as 'https://cimi/'. If you do not 
 	# specify this value it is taken from the module configuration file.
-	[Parameter(Mandatory = $false, Position = 0)]
+	[Parameter(Mandatory = $false, Position = 0, ParameterSetName = 'OAuthClientSecrect')]
+	[Parameter(ParameterSetName = "OAuthClientCred")]
 	[Uri] $OAuthBaseUrl = (Get-Variable -Name $MyInvocation.MyCommand.Module.PrivateData.MODULEVAR -ValueOnly).OAuthBaseUrl
 	,
 	# [Optional] The ApiBrokerBaseUrl such as 'https://cimiapibroker/'. If you do not 
@@ -110,7 +111,7 @@ Begin
 {
 	$datBegin = [datetime]::Now;
 	[string] $fn = $MyInvocation.MyCommand.Name;
-	Log-Debug $fn ("CALL. OAuthBaseUrl '{0}'; ApiBrokerBaseUrl '{1}'. Username '{2}'" -f $OAuthBaseUrl, $ApiBrokerBaseUrl, $Credential.Username ) -fac 1;
+	Log-Debug $fn ("CALL. ApiBrokerBaseUrl '{0}'; OAuthBaseUrl '{1}'; Username '{2}'; OAuthClientId '{3}'; TenantId '{4}'." -f $ApiBrokerBaseUrl, $OAuthBaseUrl, $Credential.Username, $OAuthClientId, $TenantId ) -fac 1;
 }
 # Begin 
 
